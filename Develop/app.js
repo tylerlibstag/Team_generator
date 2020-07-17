e093176afacf25ec518884c178389dfd092ada2f
TYTY <tylerlibstag@gmail.com>
2020-07-17 18:49:50 -0400

    updated constructors

--- Develop/app.js
+++ Develop/app.js
@@ -10,7 +10,117 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+let employeeInfo = []
 
+const questions = [
+
+    {
+        message: "Which type of employee are you?",
+        name: "role",
+        type: "list",
+        choices: ['Manager', "Intern", "Engineer"]
+    },
+    {
+        message: "What is the employee name?",
+        name: "name",
+        type: "input"
+    },
+    {
+        message: "what is the employee id?",
+        name: "id",
+        type: "input"
+    },
+    {
+        message: "what is the employee email",
+        name: "email",
+        type: "input"
+    }
+    
+
+];
+
+function moreQuestions(role, questionText, questionName, previousData) {
+    const question = [
+        {
+            message: questionText,
+            name: questionName,
+            type: "input",
+
+        },
+    ]
+    inquirer.prompt(question).then((answer) => {
+
+        const { name, id, email } = previousData;
+        
+        let employeeData;
+        
+        switch(role) {
+            case 'Manager':
+                employeeData = new Manager(name, id, email, answer);
+            break;
+            case 'Engineer':
+                employeeData = new Engineer(name, id, email, answer);
+            break;
+            case 'Intern':
+                employeeData = new Intern(name, id, email, answer);
+            break;
+        }
+        employeeInfo.push(employeeData);
+        console.log("this is employee info",employeeInfo)
+        writeHTMLtoFile() 
+
+    })
+    
+}
+
+function init() {
+
+    inquirer.prompt(questions)
+        .then((inquirerData) => {
+            const { role } = inquirerData;
+            switch (role) {
+                case 'Manager':
+                    moreQuestions(role, "officeNumber", "Enter the Manager's office number?", inquirerData);
+                    break;
+                case 'Engineer':
+                    moreQuestions(role, "github", "Enter the Engineer's Github profile username", inquirerData);
+                    break;
+                case 'Intern':
+                    moreQuestions(role, "school", "Enter where the intern goes to school", inquirerData);
+                    break;
+            }
+            
+           
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+       
+        
+}
+
+// const writeHTMLtoFile = (html) => {
+    const writeHTMLtoFile = () => {
+    
+        fs.writeFile(outputPath, render(employeeInfo), function (err) {
+
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log("Data successfully written to team.html file.");
+
+        });
+        
+    };
+init();
+
+
+
+// function promptUser(answers){
+//     const html = generateREADME(answers);
+//     fs.writeFileSync("html", html,"utf-8");
+// }
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,23 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const employee = require("./Employee")
+
+class manager extends employee{
+    constructor(name, id, email, officeNumber){
+        
+        super(name,id,email)
+      
+        this.officeNumber = officeNumber;
+        
+    }
+    getRole(){
+        return "Intern"
+    }
+    getOfficeNumber(){
+        return this.officeNumber
+    }
+    getRole() {
+        return "Manager";
+    }
+
+}
+module.exports = manager
